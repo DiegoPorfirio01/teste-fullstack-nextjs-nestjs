fea43a98ad7d7d186fb3419b4fdec1992ca9cd18
diegoviannaporfirio <porfiriodiego64@gmail.com>
2026-03-17 14:32:10 -0300

    feat: sentry implementation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG ?? "___ORG_SLUG___",
+  project: process.env.SENTRY_PROJECT ?? "___PROJECT_SLUG___",
+
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  //Sem true: alguns erros aparecem como código minificado, arquivos genéricos ou linhas estranhas
+  widenClientFileUpload: true,
+  // Proxy route to bypass ad-blockers
+  tunnelRoute: "/monitoring",
+
+  silent: !process.env.CI,
+});
